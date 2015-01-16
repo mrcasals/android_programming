97e85332d8720425663a5b8a0e8d322296206008
Marc Riera <mrc2407@gmail.com>
2015-01-16 09:53:38 +0100

    Move arguments to a Bundle

--- app/src/main/java/com/codegram/marc/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/codegram/marc/criminalintent/CrimeActivity.java
@@ -5,11 +5,16 @@ import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import java.util.UUID;
+
 
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        UUID crimeId = (UUID)getIntent()
+                .getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return new CrimeFragment().newInstance(crimeId);
     }
 }
